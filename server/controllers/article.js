9bcfeed1c2e207a5754ef2719b42abc8809c897f
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-11-09 15:00:48 +0700

    checkpoint: before break

---
+++ server/controllers/article.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const models = require('./../models');
+const genResp = require('./../helpers/gen-response');
+
+class Article {
+
+// router.get('/', (req, res) => res.send('ok'));
+
+// router.get('/:id', (req, res) => res.send('ok'));
+
+// router.get('/category/:category', (req, res) => res.send('ok'))
+
+// router.get('/author/:author', (req, res) => res.send('ok'))
+
+// router.post('/', (req, res) => res.send('ok'))
+
+// router.put('/:id', (req, res) => res.send('ok'))
+
+// router.delete('/:id', (req, res) => res.send('ok'))
+
+}
+
+module.exports = Article;

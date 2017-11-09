ea28d9812dda9719c668c0db84fbc4a5f9d58ed3
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-11-09 14:54:26 +0700

    create articles routes

--- server/app.js
+++ server/app.js
@@ -8,13 +8,15 @@ const cors = require('cors');
 
 const app = express();
 
-const index = require('./routes/index')
+const index = require('./routes/index');
+const articles = require('./routes/articles');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', index);
+app.use('/articles', articles);
 
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`hacktivpress server running on port ${port}`));
---
+++ server/routes/articles.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const express = require('express');
+const controllers = require('./../controllers');
+
+const router = express.Router();
+
+router.get('/', (req, res) => res.send('ok'));
+
+router.get('/:id', (req, res) => res.send('ok'));
+
+router.get('/:category', (req, res) => res.send('ok'))
+
+router.get('/:author', (req, res) => res.send('ok'))
+
+router.post('/', (req, res) => res.send('ok'))
+
+router.put('/:id', (req, res) => res.send('ok'))
+
+router.delete('/:id', (req, res) => res.send('ok'))
+
+module.exports = router;
